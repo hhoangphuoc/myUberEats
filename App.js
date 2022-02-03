2d48360ab08c5ff276462c7108e3a394a92d3c4f
Ho Hoang Phuoc <69688044+hhoangphuoc@users.noreply.github.com>
2022-02-03 03:37:35 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import { View, Text, SafeAreaView } from "react-native";
+import Home from "./screens/Home";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Home />
+    </View>
+  );
+}
