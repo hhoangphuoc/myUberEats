47ae620e03b8a254d2f262a2d2c85551828092c1
hhoangphuoc <hohoangphuoc2002@gmail.com>
2022-02-04 11:39:19 +0100

    create Categories component

---
+++ components/homeComponents/Categories.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Text, Image, ScrollView } from "react-native";
+
+import { dummydata } from "../../constant";
+
+export default function Categories() {
+  return (
+    <View
+      style={{
+        marginTop: 5,
+        backgroundColor: "#fff",
+        paddingVertical: 10,
+        paddingLeft: 20,
+      }}
+    >
+      <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+        {dummydata.items.map((item, index) => (
+          <View key={index} style={{ alignItems: "center", marginRight: 30 }}>
+            <Image
+              source={item.image}
+              style={{
+                width: 50,
+                height: 40,
+                resizeMode: "contain",
+              }}
+            />
+            <Text style={{ fontSize: 13, fontWeight: "900" }}>{item.text}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
