abec0978dd9f93a2ee98f4b7318e9a6413ddb6c7
hhoangphuoc <hohoangphuoc2002@gmail.com>
2022-02-06 02:30:29 +0100

    create bottom tabs and make it as home component

---
+++ components/index.js
@@ -0,0 +1,7 @@
+import HeaderTabs from "./homeComponents/HeaderTabs";
+import Categories from "./homeComponents/Categories";
+import SearchBar from "./homeComponents/SearchBar";
+import RestaurantItems from "./homeComponents/RestaurantItems";
+import BottomTabs from "./homeComponents/BottomTabs";
+
+export { HeaderTabs, Categories, SearchBar, RestaurantItems, BottomTabs };
