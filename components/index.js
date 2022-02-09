991b50e6f460051d1650124f00cc8077c0351b0e
hhoangphuoc <hohoangphuoc2002@gmail.com>
2022-02-10 00:04:44 +0100

    make order and view cart component for adding order item to cart

--- components/index.js
+++ components/index.js
@@ -3,5 +3,19 @@ import Categories from "./homeComponents/Categories";
 import SearchBar from "./homeComponents/SearchBar";
 import RestaurantItems from "./homeComponents/RestaurantItems";
 import BottomTabs from "./homeComponents/BottomTabs";
+import About from "./restaurantDetail/About";
+import MenuItems from "./restaurantDetail/MenuItems";
+import OrderItem from "./restaurantDetail/OrderItem";
+import ViewCart from "./restaurantDetail/ViewCart";
 
-export { HeaderTabs, Categories, SearchBar, RestaurantItems, BottomTabs };
+export {
+  HeaderTabs,
+  Categories,
+  SearchBar,
+  RestaurantItems,
+  BottomTabs,
+  About,
+  MenuItems,
+  OrderItem,
+  ViewCart,
+};
---
+++ screens/index.js
@@ -0,0 +1,4 @@
+import Home from "./Home";
+import RestaurantDetail from "./RestaurantDetail";
+import OrderCompleted from "./OrderCompleted";
+export { Home, RestaurantDetail, OrderCompleted };
